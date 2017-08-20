01b9dc534f2cf2a77ffd43325500698bb543dac7
BxNxPxC <poncec.briann@gmail.com>
2017-08-20 12:21:58 -0300

    Inicial proyecto
    Grupal

---
+++ WKS/Proyecto/src/proyecto/Mayor.java
@@ -0,0 +1,16 @@
+package proyecto;
+
+public class Mayor {
+	
+	private int n1;
+	private int n2;
+	private int n3;
+
+	
+	public Mayor (int n1, int n2, int n3){
+		this.n1 = n1;
+		this.n2 = n2;
+		this.n3 = n3;
+	}
+
+}
